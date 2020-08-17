7114f3efa12a4fbef641b596eba70fb955d28757
class02 <class02@192.168.0.57>
2020-08-17 18:17:35 +0900

    day23 commit

---
+++ class_02/src/day23/ex/Extra.java
@@ -0,0 +1,28 @@
+package day23.ex;
+
+import java.util.*;
+import java.io.*;
+import day23.source.*;
+
+/*
+ 	map 데이터에 키값을
+ 		사각, 삼각, 원으로 하고
+ 		
+ 		사각 일 경우 Nemo의 인스턴스의 getArea()가 호출되도록
+ 		삼각 일 경우 Semo 의 인스턴스가 getArea()가 호출되도록
+ 		원형 일 경우 Won의 인스턴스가 getArea()가 호출되도록 하고자 한다
+ 		
+ 		필요한 파일과
+ 		코드를 작성하세요...
+ */
+
+public class Extra {
+
+	public Extra() {
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
